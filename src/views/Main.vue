c40851ffa27d342442810ed80330dc97dd00084a
zhuyongji <438261437@qq.com>
2018-03-16 00:40:47 +0800

    test.3

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -25,6 +25,12 @@
 						<el-button
 							type="text"
 							size="mini"
+							@click="() => edit(node, data)">
+							编辑
+						</el-button>
+						<el-button
+							type="text"
+							size="mini"
 							@click="() => remove(node, data)">
 							删除
 						</el-button>
@@ -34,19 +40,30 @@
 			</div>
 		</div>
 		<!-- 弹出框 -->
-		<el-dialog title="添加主机构" :visible.sync="dialogFormVisible">
-			<el-form :model="form" :rules="rules">
+		<el-dialog title="添加机构" :visible.sync="dialogFormVisible">
+			<el-form :model="form" :rules="rules1"  ref="form">
 				<el-form-item label="机构名称" :label-width="formLabelWidth" prop="name">
 					<el-input type="text" v-model="form.name" placeholder="请输入机构名"></el-input>
 				</el-form-item>
 			</el-form>
 			<div slot="footer" class="dialog-footer">
-				<el-button @click="dialogFormVisible = false,parentNode = false,this.childrenNode = false">取 消</el-button>
-				<el-button type="primary" @click ="add(form.name)">添 加</el-button>
+				<el-button @click="dialogFormVisible = false,parentNode = false,childrenNode = false,form.name = ''">取 消</el-button>
+				<el-button type="primary" @click ="add('form')">添 加</el-button>
+			</div>
+		</el-dialog>
+		<el-dialog title="编辑机构" :visible.sync="dialogEditVisible">
+			<el-form :model="Edit" :rules="rules2" ref="Edit">
+				<el-form-item label="机构名称" :label-width="formLabelWidth" prop="name">
+					<el-input type="text" v-model="Edit.name" placeholder="请修改机构名"></el-input>
+				</el-form-item>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button @click="dialogEditVisible = false,Edit.name = ''">取 消</el-button>
+				<el-button type="primary" @click ="SaveEdit('Edit')">修 改</el-button>
 			</div>
 		</el-dialog>
 		<!-- 确认删除提示框 -->
-		<el-dialog
+		<!--<el-dialog
 			title="提示"
 			:visible.sync="dialogVisible"
 			width="30%"
@@ -56,7 +73,7 @@
 				<el-button @click="dialogVisible = false">取 消</el-button>
 				<el-button type="primary" @click="dialogVisible = false">确 定</el-button>
 			</span>
-		</el-dialog>
+		</el-dialog> -->
 	</div>
 </template>
 
@@ -77,34 +94,36 @@
 		data () {
 			const data = localData.getData('data-list');
 			return {
+				EditIndex:'',
+				EditData:[],
 				childrenData:[],
 				disabl:true,
 				parentNode:false,
 				childrenNode:false,
 				dialogVisible:false,
+				dialogEditVisible:false,
 				dialogFormVisible: false,
 				formLabelWidth: '80px',
 				dataList: JSON.parse(JSON.stringify(data)),
-				form:{
-					name:''
+				form:{ name:'' },
+				Edit:{ name:'' },
+				rules1: {
+					name: [{ required: true, message: '请输入机构名称', trigger: 'blur' }]
 				},
-				rules: {
-					name: [
-						{ required: true, message: '请输入机构名称', trigger: 'blur' }
-					]
+				rules2: {
+					name: [{ required: true, message: '机构名称必填', trigger: 'blur' }]
 				}
 			};
 		},
 		watch:{
-				//监听的动作为 当todoList发生变化的时候 执行回调函数
-				//深监控
-				dataList:{
-					handler:function(){
-						localData.save("data-list",this.dataList);
-					}, 
-					deep:true
-				}
-			},
+			//深监控
+			dataList:{
+				handler:function(){
+					localData.save("data-list",this.dataList);
+				}, 
+				deep:true
+			}
+		},
 		methods: {
 			append(data) {
 				this.parentNode        = false;
@@ -115,8 +134,28 @@
 			remove(node, data) {
 				const parent   = node.parent;
 				const children = parent.data.children || parent.data;
-				const index = children.findIndex(d => d.id === data.id);
+				const index    = children.findIndex(d => d.id === data.id);
 				children.splice(index, 1);
+			},
+			edit(node, data) {
+				const parent   = node.parent;
+				const children = parent.data.children || parent.data;
+				this.dialogEditVisible = true;
+				this.Edit.name         = data.label;
+				this.EditIndex         = children.findIndex(d => d.id === data.id);
+				this.EditData          = children;
+			},
+			//保持编辑
+			SaveEdit(formName) {
+				 this.$refs[formName].validate((valid) => {
+					 if(valid) {
+						 this.EditData[this.EditIndex].label = this.Edit.name;
+						 this.dialogEditVisible = false;
+						 this.Edit.name = '';
+					 }else {
+						 return false;
+					 }
+				 })
 			},
 			//按钮添加主机构
 			BtnAdd() {
@@ -126,24 +165,26 @@
 				let name = this.form.name;
 			},
 			add(formName) {
-				if(formName){
-					if(this.parentNode){
-						let newChild   = { id: this.dataList.length+1, label:formName ,children : [] };
-						this.dataList.push(newChild);
-					}
-					if(this.childrenNode){
-						let newChild = { id: this.childrenData*10+1, label: formName, children: [] };
-						if (!this.childrenData.children) {
-							this.$set(this.childrenData, 'children', []);
+				this.$refs[formName].validate((valid) => {
+					if(valid){
+						if(this.parentNode){
+							let newChild   = { id: this.dataList.length+1, label:this.form.name ,children : [] };
+							this.dataList.push(newChild);
 						}
-						this.childrenData.children.push(newChild);
+						if(this.childrenNode){
+							let newChild = { id: (this.childrenData.children.length+1)*10+1, label: this.form.name, children: [] };
+							if (!this.childrenData.children) {
+								this.$set(this.childrenData, 'children', []);
+							}
+							this.childrenData.children.push(newChild);
+						}
+						this.form.name = '';
+						this.dialogFormVisible = false;
+					}else {
+						// this.$message.error('必填，请输入机构名！！！');
+						return false;
 					}
-					this.form.name = '';
-					this.dialogFormVisible = false;
-				}else {
-					this.$message.error('机构名必填，请输入！！！');
-					return false;
-				}
+        		});
 			}
 		}
 	}
--- src/views/nav1/add.vue
+++ src/views/nav1/add.vue
@@ -1,12 +1,362 @@
 <template>
 	<div>
-  	
+		<el-button style="margin-top:15px;" @click="dialogAddVisible = true">添加设备</el-button>
+		<el-table
+			:data="tableData"
+			border
+			style="width: 100%;margin-top:15px;">
+				<el-table-column
+					prop="name"
+					label="设备名称"
+					width="180">
+				</el-table-column>
+				<el-table-column
+					prop="number"
+					label="设备编号"
+					width="180">
+				</el-table-column>
+				<el-table-column
+					prop="networkType"
+					label="网络类型">
+				</el-table-column>
+				<el-table-column
+					prop="phone"
+					label="手机号">
+				</el-table-column>
+				<el-table-column
+					prop="line"
+					label="经纬度">
+				</el-table-column>
+				<el-table-column
+					prop="tissue"
+					label="所属机构">
+				</el-table-column>
+				 <el-table-column
+					fixed="right"
+					label="操作"
+					width="100">
+					<template slot-scope="scope">
+					<el-tooltip content="编辑设备" placement="top" effect="dark">
+						<el-button type="text" size="small"><i style="font-size:20px;" class="el-icon-edit"></i></el-button>
+					</el-tooltip>
+					<el-tooltip content="删除设备" placement="top" effect="dark">
+						<el-button type="text" size="small"><i style="font-size:20px;" class="el-icon-delete"></i></el-button>
+					</el-tooltip>
+					</template>
+				</el-table-column>
+		</el-table>
+		 <el-pagination
+		 style="margin-top:15px;float:right;"
+		 	background
+			@current-change="handleCurrentChange"
+			:page-size="10"
+			layout="total, prev, pager, next, jumper"
+			:total="pageInfo.total">
+		</el-pagination>
+		<!-- 添加设备 弹窗 -->
+		<el-dialog title="添加设备" :visible.sync="dialogAddVisible">
+			<el-form :model="form" :label-position="'left'">
+				<el-row :gutter="60">
+					<el-col :span="8">
+						<div class="grid-content bg-purple">
+							<el-form-item label="设备编号" :label-width="formLabelWidth">
+								<el-input v-model="form.number" auto-complete="off"></el-input>
+							</el-form-item>
+						</div>
+					</el-col>
+					<el-col :span="8">
+						<div class="grid-content bg-purple-light">
+							<el-form-item label="设备名称" :label-width="formLabelWidth">
+								<el-input v-model="form.name" auto-complete="off"></el-input>
+							</el-form-item>
+						</div>
+					</el-col>
+				</el-row>
+				<el-row :gutter="60">
+					<el-col :span="8">
+						<div class="grid-content bg-purple">
+							<el-form-item label="网络类型" :label-width="formLabelWidth">
+								<el-select v-model="form.networkType" placeholder="请选择网络类型">
+									<el-option label="无线" value="wuxian"></el-option>
+									<el-option label="有线" value="youxian"></el-option>
+								</el-select>
+    						</el-form-item>
+						</div>
+					</el-col>
+					<el-col :span="8">
+						<div class="grid-content bg-purple-light">
+							<el-form-item label="手机号" :label-width="formLabelWidth">
+								<el-input v-model="form.phone" auto-complete="off"></el-input>
+							</el-form-item>
+						</div>
+					</el-col>
+				</el-row>
+				<el-row >
+					<el-col :span="6">
+						<div class="grid-content bg-purple">
+							<el-form-item label="经纬度" :label-width="formLabelWidth">
+								<el-input v-model="form.name" auto-complete="off"></el-input>
+							</el-form-item>
+							<el-form-item label="" :label-width="formLabelWidth">
+								<el-input v-model="form.name" auto-complete="off"></el-input>
+							</el-form-item>
+							<el-form-item label="" :label-width="formLabelWidth">
+								<el-button type="primary" plain>选择</el-button>
+							</el-form-item>
+						</div>
+					</el-col>
+				</el-row>
+				<el-row :gutter="60">
+					<el-col :span="8">
+						<div class="grid-content bg-purple">
+							<el-form-item label="所属机构" :label-width="formLabelWidth">
+								<!-- <el-input v-model="form.name" auto-complete="off"></el-input> -->
+								<el-cascader
+								:options="options"
+								:show-all-levels="false"
+								v-model="form.tissue"
+								></el-cascader>
+							</el-form-item>
+						</div>
+					</el-col>
+				</el-row>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button @click="dialogAddVisible = false">取 消</el-button>
+				<el-button type="primary" @click="dialogAddVisible = false">确 定</el-button>
+			</div>
+		</el-dialog>
     </div>
 </template>
 
 <script>
-    import { msgData } from '../../api/api';
+	import { msgData } from '../../api/api';
+	let testList = [
+					{
+						id:'1',
+						number: '001',
+						name: '设备1',
+						networkType: '有线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}, 
+					{
+						id:'2',
+						number: '002',
+						name: '设备2',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}, 
+					{
+						id:'3',
+						number: '003',
+						name: '设备3',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}, 
+					{
+						id:'4',
+						number: '004',
+						name: '设备4',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}, 
+					{
+						id:'5',
+						number: '005',
+						name: '设备005',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}, 
+					{
+						id:'4',
+						number: '006',
+						name: '设备006',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}, 
+					{
+						id:'4',
+						number: '007',
+						name: '设备007',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}, 
+					{
+						id:'4',
+						number: '008',
+						name: '设备008',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}, 
+					{
+						id:'4',
+						number: '009',
+						name: '设备009',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}, 
+					{
+						id:'4',
+						number: '010',
+						name: '设备010',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}, 
+					{
+						id:'4',
+						number: '011',
+						name: '设备011',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}, 
+					{
+						id:'4',
+						number: '012',
+						name: '设备012',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}, 
+					{
+						id:'4',
+						number: '013',
+						name: '设备013',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					},
+					{
+						id:'4',
+						number: '014',
+						name: '设备014',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					},
+					{
+						id:'4',
+						number: '015',
+						name: '设备015',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					},
+					{
+						id:'4',
+						number: '016',
+						name: '设备016',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					},
+					{
+						id:'4',
+						number: '017',
+						name: '设备017',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					},
+					{
+						id:'4',
+						number: '018',
+						name: '设备018',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					},
+					{
+						id:'4',
+						number: '019',
+						name: '设备19',
+						networkType: '无线',
+						phone:'18079071115',
+						line:'-23.03,-113.75',
+						tissue:'机构1'
+					}
+				]
 	export default {
-		
+		 data() {
+			const total       = testList.length;
+			const currentPage = Math.floor(total/10); 
+			return {
+				formLabelWidth:'80px',
+				dialogAddVisible:false,
+				pageInfo:{
+					total:total,
+					currentPage: currentPage,
+				},
+				form:{
+					number: '',
+					name: '',
+					networkType: '',
+					phone:'',
+					line:'',
+					tissue:''
+				},
+				tableData: [],
+				options: [{
+					value: 'zhinan',
+					label: '指南',
+					children: [{
+						value: 'shejiyuanze',
+						label: '设计原则',
+						children: [{
+							value: 'yizhi',
+							label: '一致'
+						}, {
+							value: 'fankui',
+							label: '反馈'
+						}, {
+							value: 'xiaolv',
+							label: '效率'
+						}, {
+							value: 'kekong',
+							label: '可控'
+						}]
+					}]
+				}]
+			}
+		},
+		created() {
+			this.tableData = testList.slice(0,10);
+		},
+		methods: {
+			handleCurrentChange(val) {
+				console.log(`当前页: ${val}`);
+				const countStart = val*10 -10;
+        		const countEnd   = val*10;
+				this.tableData= testList.slice(countStart,countEnd)
+			}
+		},
 	}
 </script>
