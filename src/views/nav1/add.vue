ee19d22153d20c77984fa6d40314c64c42c81c19
zhuyongji <438261437@qq.com>
2018-03-16 03:26:27 +0800

    test.4

--- src/views/nav1/add.vue
+++ src/views/nav1/add.vue
@@ -24,7 +24,7 @@
 					label="手机号">
 				</el-table-column>
 				<el-table-column
-					prop="line"
+					prop="lineStart"
 					label="经纬度">
 				</el-table-column>
 				<el-table-column
@@ -55,18 +55,18 @@
 		</el-pagination>
 		<!-- 添加设备 弹窗 -->
 		<el-dialog title="添加设备" :visible.sync="dialogAddVisible">
-			<el-form :model="form" :label-position="'left'">
+			<el-form :model="form" :rules="rules" ref="form" :label-position="'right'">
 				<el-row :gutter="60">
 					<el-col :span="8">
 						<div class="grid-content bg-purple">
-							<el-form-item label="设备编号" :label-width="formLabelWidth">
+							<el-form-item label="设备编号" :label-width="formLabelWidth" prop="number">
 								<el-input v-model="form.number" auto-complete="off"></el-input>
 							</el-form-item>
 						</div>
 					</el-col>
 					<el-col :span="8">
 						<div class="grid-content bg-purple-light">
-							<el-form-item label="设备名称" :label-width="formLabelWidth">
+							<el-form-item label="设备名称" :label-width="formLabelWidth" prop="name">
 								<el-input v-model="form.name" auto-complete="off"></el-input>
 							</el-form-item>
 						</div>
@@ -75,7 +75,7 @@
 				<el-row :gutter="60">
 					<el-col :span="8">
 						<div class="grid-content bg-purple">
-							<el-form-item label="网络类型" :label-width="formLabelWidth">
+							<el-form-item label="网络类型" :label-width="formLabelWidth" prop="networkType">
 								<el-select v-model="form.networkType" placeholder="请选择网络类型">
 									<el-option label="无线" value="wuxian"></el-option>
 									<el-option label="有线" value="youxian"></el-option>
@@ -85,8 +85,8 @@
 					</el-col>
 					<el-col :span="8">
 						<div class="grid-content bg-purple-light">
-							<el-form-item label="手机号" :label-width="formLabelWidth">
-								<el-input v-model="form.phone" auto-complete="off"></el-input>
+							<el-form-item label="手机号" :label-width="formLabelWidth" >
+								<el-input @blur="verifyPhone()" v-model="form.phone" auto-complete="off"></el-input>
 							</el-form-item>
 						</div>
 					</el-col>
@@ -94,11 +94,11 @@
 				<el-row >
 					<el-col :span="6">
 						<div class="grid-content bg-purple">
-							<el-form-item label="经纬度" :label-width="formLabelWidth">
-								<el-input v-model="form.name" auto-complete="off"></el-input>
+							<el-form-item label="经纬度" :label-width="formLabelWidth" prop="lineStart">
+								<el-input @blur="verifyLine" v-model="form.lineStart" auto-complete="off"></el-input>
 							</el-form-item>
-							<el-form-item label="" :label-width="formLabelWidth">
-								<el-input v-model="form.name" auto-complete="off"></el-input>
+							<el-form-item label="" :label-width="formLabelWidth" prop="lineEnd">
+								<el-input @blur="verifyLine" v-model="form.lineEnd" auto-complete="off"></el-input>
 							</el-form-item>
 							<el-form-item label="" :label-width="formLabelWidth">
 								<el-button type="primary" plain>选择</el-button>
@@ -109,12 +109,12 @@
 				<el-row :gutter="60">
 					<el-col :span="8">
 						<div class="grid-content bg-purple">
-							<el-form-item label="所属机构" :label-width="formLabelWidth">
+							<el-form-item label="所属机构" :label-width="formLabelWidth" prop="tissue">
 								<!-- <el-input v-model="form.name" auto-complete="off"></el-input> -->
-								<el-cascader
+								<el-cascader  v-model="form.tissue"
 								:options="options"
 								:show-all-levels="false"
-								v-model="form.tissue"
+								change-on-select
 								></el-cascader>
 							</el-form-item>
 						</div>
@@ -123,7 +123,7 @@
 			</el-form>
 			<div slot="footer" class="dialog-footer">
 				<el-button @click="dialogAddVisible = false">取 消</el-button>
-				<el-button type="primary" @click="dialogAddVisible = false">确 定</el-button>
+				<el-button type="primary" @click="Save('form')">确 定</el-button>
 			</div>
 		</el-dialog>
     </div>
@@ -131,6 +131,41 @@
 
 <script>
 	import { msgData } from '../../api/api';
+	let verify = {
+		phone (number){
+			console.log(number)
+			var flag;
+			var myreg = /^[1][1,2,3,4,5,7,8,9][0-9]{9}$/;  
+			if (!myreg.test(number)) {  
+				return false;  
+			} else {  
+				return  true;  
+			}
+			// return flag;
+		},
+		//经度
+		lineStart(number){
+			var flag;
+			var myreg = /^[\-\+]?(0?\d{1,2}\.\d{1,5}|1[0-7]?\d{1}\.\d{1,5}|180\.0{1,5})$/;
+			if (!myreg.test(number)) {  
+				flag = false;  
+			} else {  
+				flag = true;  
+			}
+			return flag;
+		},
+		//纬度
+		lineEnd(number){
+			var flag;
+			var myreg = /^[\-\+]?([0-8]?\d{1}\.\d{1,5}|90\.0{1,5})$/;
+			if (!myreg.test(number)) {  
+				flag = false;  
+			} else {  
+				flag = true;  
+			}
+			return flag;
+		}
+	}
 	let testList = [
 					{
 						id:'1',
@@ -138,7 +173,8 @@
 						name: '设备1',
 						networkType: '有线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}, 
 					{
@@ -147,7 +183,8 @@
 						name: '设备2',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}, 
 					{
@@ -156,7 +193,8 @@
 						name: '设备3',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}, 
 					{
@@ -165,7 +203,8 @@
 						name: '设备4',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}, 
 					{
@@ -174,7 +213,8 @@
 						name: '设备005',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}, 
 					{
@@ -183,7 +223,8 @@
 						name: '设备006',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}, 
 					{
@@ -192,7 +233,8 @@
 						name: '设备007',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}, 
 					{
@@ -201,7 +243,8 @@
 						name: '设备008',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}, 
 					{
@@ -210,7 +253,8 @@
 						name: '设备009',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}, 
 					{
@@ -219,7 +263,8 @@
 						name: '设备010',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}, 
 					{
@@ -228,7 +273,8 @@
 						name: '设备011',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}, 
 					{
@@ -237,7 +283,8 @@
 						name: '设备012',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}, 
 					{
@@ -246,7 +293,8 @@
 						name: '设备013',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					},
 					{
@@ -255,7 +303,8 @@
 						name: '设备014',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					},
 					{
@@ -264,7 +313,8 @@
 						name: '设备015',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					},
 					{
@@ -273,7 +323,8 @@
 						name: '设备016',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					},
 					{
@@ -282,7 +333,8 @@
 						name: '设备017',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					},
 					{
@@ -291,7 +343,8 @@
 						name: '设备018',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					},
 					{
@@ -300,7 +353,8 @@
 						name: '设备19',
 						networkType: '无线',
 						phone:'18079071115',
-						line:'-23.03,-113.75',
+						lineStart:'23.03',
+						lineEnd:'113.75',
 						tissue:'机构1'
 					}
 				]
@@ -320,31 +374,52 @@
 					name: '',
 					networkType: '',
 					phone:'',
-					line:'',
-					tissue:''
+					lineStart:'',
+					lineEnd:'',
+					tissue:[]
 				},
-				tableData: [],
+				tableData: testList,
 				options: [{
-					value: 'zhinan',
-					label: '指南',
+					value: '机构一',
+					label: '机构一',
 					children: [{
-						value: 'shejiyuanze',
-						label: '设计原则',
+						value: '二级机构',
+						label: '二级机构',
 						children: [{
-							value: 'yizhi',
-							label: '一致'
-						}, {
-							value: 'fankui',
-							label: '反馈'
-						}, {
-							value: 'xiaolv',
-							label: '效率'
-						}, {
-							value: 'kekong',
-							label: '可控'
+							value: '三级机构',
+							label: '三级机构'
+						},{
+							value: '三级机构',
+							label: '三级机构'
+						},{
+							value: '三级机构',
+							label: '三级机构'
+						},{
+							value: '三级机构',
+							label: '三级机构'
 						}]
 					}]
-				}]
+				}],
+				rules: {
+					name: [
+						{ required: true, message: '请输入设备名称', trigger: 'blur' }
+					],
+					number: [
+						{ required: true, message: '请输入设备编号', trigger: 'blur' }
+					],
+					networkType: [
+						{ required: true, message: '请选择网络类型', trigger: 'change' }
+					],
+					lineStart: [
+						{ required: true, message: '请输入经度', trigger: 'blur' }
+					],
+					lineEnd: [
+						{ required: true, message: '请输入纬度', trigger: 'blur' }
+					],
+					tissue: [
+						{ required: true, message: '请选择所属机构', trigger: 'change' }
+					]
+            	}
 			}
 		},
 		created() {
@@ -356,6 +431,48 @@
 				const countStart = val*10 -10;
         		const countEnd   = val*10;
 				this.tableData= testList.slice(countStart,countEnd)
+			},
+			Save(form) {
+				this.$refs[form].validate((valid) => {
+					let flag1= verify.phone(this.form.phone);
+					let flag2= verify.lineStart(this.form.lineStart);
+					let flag3= verify.lineEnd(this.form.lineEnd);
+					console.log(flag1)
+					if (valid && flag1 && flag2 && flag3) {
+						testList.push({
+							number: this.form.number,
+							name: this.form.name,
+							networkType: this.form.networkType,
+							phone:this.form.phone,
+							lineStart:this.form.lineStart,
+							lineEnd:this.form.lineEnd,
+							tissue:this.form.tissue
+						})
+						this.tableData = testList.slice(0,10);
+						this.pageInfo.total = testList.length;
+						this.dialogAddVisible = false;
+						this.form = {};
+					} else {
+						console.log('error submit!!');
+						return false;
+					}
+				});
+			},
+			verifyPhone() {
+				let flag1= verify.phone(this.form.phone);
+				if(!flag1){
+					this.$message.error('手机号格式不正确！！！');
+				}
+			},
+			verifyLine(form) {
+				let flag1= verify.lineStart(this.form.lineStart);
+				let flag2= verify.lineEnd(this.form.lineEnd);
+				if(!flag1){
+					this.$message.error('经度格式不正确！！！');
+				}
+				if(!flag2){
+					this.$message.error('纬度格式不正确！！！');
+				}
 			}
 		},
 	}
